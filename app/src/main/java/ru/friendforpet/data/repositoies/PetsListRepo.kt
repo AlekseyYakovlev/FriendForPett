b0ab5c14dfc32809679ace85fa05c115525aaa93
NEMM <52427720+NEMMFIELD@users.noreply.github.com>
2021-03-12 22:26:13 +0300

    Болванка

--- app/src/main/java/ru/friendforpet/data/repositoies/PetsListRepo.kt
+++ app/src/main/java/ru/friendforpet/data/repositoies/PetsListRepo.kt
@@ -1,4 +1,22 @@
 package ru.friendforpet.data.repositoies
 
+import ru.friendforpet.model.Pet
+
 class PetsListRepo {
+    fun petListRepoMog(): Pet = Pet(
+        141983,
+        "Мухтар",
+        "Мальчик",
+        3,
+        "Москва",
+        "Большая",
+        "Дружелюбная",
+        "Длинношерстная",
+        "Черно-рыжий",
+        "Отзывчивый, обучаемый, спокойный, ладит с детьми",
+        listOf(
+            "Дружелюбный", "Приучен к поводку", "Без агрессии", "Овчарка"
+        ),
+        "23.03.2021"
+    )
 }
