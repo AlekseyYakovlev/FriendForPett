1deffd34129c91f79db9e9bc1637ee3822a1da7e
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-03-13 20:45:06 +0300

    WelcomeScreen updated

--- app/src/main/java/ru/friendforpet/ui/pets_list/WelcomeScreenFragment.kt
+++ app/src/main/java/ru/friendforpet/ui/pets_list/WelcomeScreenFragment.kt
@@ -4,13 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
 import ru.friendforpet.Navigator
 import ru.friendforpet.R
 import ru.friendforpet.databinding.FragmentWelcomeScreenBinding
 import ru.friendforpet.ui.utils.viewBinding
 import javax.inject.Inject
 
-class WelcomeScreenFragment  : Fragment(R.layout.fragment_welcome_screen) {
+@AndroidEntryPoint
+class WelcomeScreenFragment : Fragment(R.layout.fragment_welcome_screen) {
     private val viewModel: PetsListViewModel by viewModels()
     private val vb by viewBinding(FragmentWelcomeScreenBinding::bind)
 
@@ -25,6 +27,12 @@ class WelcomeScreenFragment  : Fragment(R.layout.fragment_welcome_screen) {
     private fun setupViews() {
 
 
+
+        vb.mbSubmit.setOnClickListener {
+            navigator.navigateTo(Navigator.Destination.PETS_LIST_FRAGMENT)
+        }
+
+        vb.cvCat.setOnClickListener {  }
 
     }
 }
