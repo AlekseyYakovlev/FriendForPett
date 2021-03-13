3ff1b632a9c54ceef15422942ae318d83e42603d
Vyacheslav <revan@xakep.ru>
2021-03-13 10:56:26 +0300

    Fix TypeConverter

--- app/src/main/java/ru/friendforpet/data/db/ListTypeConverter.kt
+++ app/src/main/java/ru/friendforpet/data/db/ListTypeConverter.kt
@@ -5,7 +5,7 @@ import androidx.room.TypeConverter
 
 class ListTypeConverter {
     @TypeConverter
-    fun dbToList(string: String): List<String> = string.split("[,]".toRegex()).toList()
+    fun dbToList(string: String): List<String> = string.split("[■]".toRegex()).toList()
 
     @TypeConverter
     fun listToDb(list: List<String>): String = list.joinToString(separator = "■")
