f2cc9f9d1c42e319b79c8eafae0011de235661d3
Aleksey <31004501+AlekseyYakovlev@users.noreply.github.com>
2021-03-13 22:24:30 +0300

    Filter updated

--- app/src/main/java/ru/friendforpet/ui/pets_list/FilterFragment.kt
+++ app/src/main/java/ru/friendforpet/ui/pets_list/FilterFragment.kt
@@ -2,17 +2,17 @@ package ru.friendforpet.ui.pets_list
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import ru.friendforpet.R
 import ru.friendforpet.databinding.FragmentPetsListFilterBinding
-import ru.friendforpet.ui.pets_details.PetsDetailsViewModel
 import ru.friendforpet.ui.utils.viewBinding
 
 @AndroidEntryPoint
 class FilterFragment : Fragment(R.layout.fragment_pets_list_filter) {
-    private val viewModel: PetsDetailsViewModel by viewModels()
+    private val viewModel: PetsListViewModel by viewModels()
     private val vb by viewBinding(FragmentPetsListFilterBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -20,6 +20,38 @@ class FilterFragment : Fragment(R.layout.fragment_pets_list_filter) {
         setupViews()
     }
     private fun setupViews() {
+        vb.btClean.setOnClickListener {
+            viewModel.cleanFilter()
+            setFilterValuesToDefault()
+        }
+
+        vb.spLocation.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            viewModel.updateFilter("location", "")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+               val location = vb.spLocation.getItemAtPosition(position).toString()
+                viewModel.updateFilter("location", location)
+            }
+
+        }
+
+        vb.spPetType.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                viewModel.updateFilter("type", "")
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+                val location = vb.spPetType.getItemAtPosition(position).toString()
+                viewModel.updateFilter("type", location)
+            }
+
+        }
+
+    }
+
+    private fun setFilterValuesToDefault(){
 
     }
 
--- app/src/main/java/ru/friendforpet/ui/pets_list/PetsListViewModel.kt
+++ app/src/main/java/ru/friendforpet/ui/pets_list/PetsListViewModel.kt
@@ -4,10 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import ru.friendforpet.data.repositoies.PetsListRepo
 import ru.friendforpet.model.Pet
@@ -19,12 +16,15 @@ class PetsListViewModel @Inject constructor(
     private val petsListRepo: PetsListRepo,
 ) : ViewModel() {
 
-    var pickedCity : String = ""
+    var pickedCity: String = ""
+
+    private val _filterState = MutableStateFlow(Filter())
+
 
     init {
-       viewModelScope.launch (Dispatchers.IO){
-           petsListRepo.insertInitialValues()
-       }
+        viewModelScope.launch(Dispatchers.IO) {
+            petsListRepo.insertInitialValues()
+        }
     }
 
     private val filtersMap = mutableMapOf<String, String>()
@@ -38,15 +38,58 @@ class PetsListViewModel @Inject constructor(
             }
 
     fun handleLike(petId: Int, isLiked: Boolean) {
-        viewModelScope.launch (Dispatchers.IO){
-            petsListRepo.updateIsLiked(petId,isLiked)
+        viewModelScope.launch(Dispatchers.IO) {
+            petsListRepo.updateIsLiked(petId, isLiked)
         }
     }
 
-    fun updateFilter(){
+    fun updateFilter(field: String, value: String) {
+        when (field) {
+            "location" -> {
+                _filterState.value = _filterState.value.copy(location = value)
+                Timber.tag("123").d("location = $value")
+            }
+            "type" -> {
+                _filterState.value = _filterState.value.copy(type = value)
+                Timber.tag("123").d("type = $value")
+            }
+            "gender" -> {
+                _filterState.value = _filterState.value.copy(gender = value)
+            }
+            "age" -> {
+                _filterState.value = _filterState.value.copy(age = value)
+            }
+            "size" -> {
+                _filterState.value = _filterState.value.copy(size = value)
+            }
+            "personality" -> {
+                _filterState.value = _filterState.value.copy(personality = value)
+            }
+            "hair" -> {
+                _filterState.value = _filterState.value.copy(hair = value)
+            }
+            "color" -> {
+                _filterState.value = _filterState.value.copy(color = value)
+            }
+        }
+    }
 
+    fun cleanFilter(){
+        _filterState.value = Filter()
     }
 }
+
+data class Filter(
+    val location: String = "",
+    val type: String = "",
+    val gender: String = "",
+    val age: String = "",
+    val size: String = "",
+    val personality: String = "",
+    val hair: String = "",
+    val color: String = "",
+)
+
 
 fun Pet.toPetItemData() = PetsItemData(
     _id = _id,
